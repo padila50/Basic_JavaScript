407d59077546dca411985b9489aab1068234bab1
PADILA <padila.tgbinga@gmail.com>
2018-04-20 15:49:32 +0700

    Create isi.js

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<CENTER>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("<H1>UNIVERSITAS KOMPUTER INDONESIA</H1>");
+document.writeln("<H2>FAKULTAS TEKNIK dan ILMU KOMPUTER<H2>");document.writeln("<H3>JURUSAN TEKNIK INFORMATIKA<H3>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("</CENTER>");
+ 
